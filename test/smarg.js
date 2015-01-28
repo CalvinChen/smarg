5af90e9049e1c7ace6cd916e7e10510eeca8e216
CalvinChen <Calvin.Chen.310@gmail.com>
2015-01-29 01:34:37 +0800

    init

--- test/smarg.js
+++ test/smarg.js
@@ -2,40 +2,34 @@
  * Created by Calvin Chen on 2015/1/28.
  */
 var Smarg = require('../lib/smarg');
+var $ = Smarg.$;
 var expect = require('chai').expect;
-var argu = new Smarg();
 describe('smarge', function() {
-    describe('#ifOnlyCb', function() {
+    describe('#hasCb', function() {
         function funWith2argus(argu1, cb) {
-            argu.ifOnlyCb(arguments, 2);
+            $(arguments).hasCb();
             return arguments;
         }
         function funWith6argus(argu1, argu2, argu3, argu4, argu5, cb) {
-            argu.ifOnlyCb(arguments, 6);
+            $(arguments).hasCb();
             return arguments;
         }
-        function fun(argu1, cb) {
-            argu.ifOnlyCb(arguments);
-        }
         function cb() {}
-        it('should throw an error if the second argument was not set', function() {
-            expect(fun).to.throw(Error);
-        });
         it('should change the arguments which has only one callback', function() {
             var argus = funWith2argus();
             expect(argus.length).to.equal(0);
             argus = funWith2argus(cb);
-            expect(argus[0]).to.be.null;
+            expect(argus[0]).to.be.undefined;
             expect(argus[1]).to.equal(cb);
             argus = funWith2argus(1, cb);
             expect(argus[1]).to.equal(cb);
 
             argus = funWith6argus(cb);
-            expect(argus[0]).to.be.null;
-            expect(argus[1]).to.be.null;
-            expect(argus[2]).to.be.null;
-            expect(argus[3]).to.be.null;
-            expect(argus[4]).to.be.null;
+            expect(argus[0]).to.be.undefined;
+            expect(argus[1]).to.be.undefined;
+            expect(argus[2]).to.be.undefined;
+            expect(argus[3]).to.be.undefined;
+            expect(argus[4]).to.be.undefined;
             expect(argus[5]).to.equal(cb);
 
             argus = funWith6argus(1, 2, 3, 4, 5, cb);
