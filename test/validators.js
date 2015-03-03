71d99b6177e5d9dd6f1e1fe0cf4d006f977957d0
Calvin Chen <244739873@qq.com>
2015-03-03 17:37:28 +0800

    init

--- test/validators.js
+++ test/validators.js
@@ -6,6 +6,25 @@ var $ = Smarg.$;
 var expect = require('chai').expect;
 function cb() {}
 function cb1() {return 1;}
+function shouldNotThrowError(func, done) {
+    try {
+        func();
+        if(done) done();
+    } catch (e) {
+        expect(e).to.be.undefined;
+    }
+}
+function shouldThrowError(func, done) {
+    var sthWrong = false;
+    try {
+        func();
+        sthWrong = true;
+    } catch (e) {
+        expect(e).to.be.not.undefined;
+    }
+    expect(sthWrong).to.equal(false);
+    if(done) done();
+}
 describe('Smarg.addValidators', function () {
     it('should be invokable after adding new validators', function (done) {
         // newValiName = 'is0to120';
@@ -15,94 +34,121 @@ describe('Smarg.addValidators', function () {
             if(param >= 0 && param <= 120) return true;
             return false;
         }, errorMsg);
-        expect(Smarg.is0to120).not.to.be.undefined;
         var testFunc = function (num) {
             $(num).is0to120();
             return true;
         };
-
+        shouldNotThrowError(function () {
+            testFunc(45);
+        });
+        shouldThrowError(function () {
+            testFunc(121);
+        });
+        done();
     });
 });
 describe('smarg.validators', function() {
-    describe('#require', function () {
-        function require1(argu1, argu2) {
-            $(arguments).require(argu2);
-        }
-        it('should work fine when #require meet not false value', function (done) {
-            try {
-                require1(null, "some value");
-                require1(null, false);
-                require1(null, true);
-                require1(null, "");
-                require1(null, 0);
-                done();
-            } catch (e) {
-                expect(e).to.be.undefined;
-            }
+    describe('#isRequired', function () {
+        doTest(function(argu1, argu2) {
+            $(arguments).isRequired(argu2);
         });
-        it('should throw error when #require meet false value', function (done) {
-            var falsyValus = [null, undefined, NaN]; // except zero, false and empty string
-            for(var i = 0; i < falsyValus.length; i++) {
-                var isError = false;
-                try {
-                    require1("some value", falsyValus[i]);
-                    isError = true;
-                } catch (e) {
-                    expect(e).to.be.not.undefined;
+
+        doTest(function(argu1, argu2) {
+            $(argu2).isRequired();
+        });
+
+        doTest(function(argu1, argu2) {
+            $(argu2).isRequired;
+        });
+
+        function doTest(requireArgu2) {
+            it('should work fine when #require meet not false value', function (done) {
+                shouldNotThrowError(function () {
+                    requireArgu2(null, "some value");
+                    requireArgu2(null, false);
+                    requireArgu2(null, true);
+                    requireArgu2(null, "");
+                    requireArgu2(null, 0);
+                }, done);
+            });
+            it('should throw error when #require meet false value', function (done) {
+                var falsyValus = [null, undefined, NaN]; // except zero, false and empty string
+                for(var i = 0; i < falsyValus.length; i++) {
+                    shouldThrowError(function(){
+                        requireArgu2("some value", falsyValus[i]);
+                    });
                 }
-                expect(isError).to.equal(false);
-            }
-            done();
+                done();
+            });
+        }
+    });
+    describe('#areRequired', function () {
+        doTest(function(argu1, argu2) {
+            $(arguments).areRequired([argu1, argu2]);
         });
+
+        doTest(function(argu1, argu2) {
+            $([argu1, argu2]).areRequired();
+        });
+
+        doTest(function(argu1, argu2) {
+            $([argu1, argu2]).areRequired;
+        });
+
+        function doTest(requireBoth) {
+            it('should work fine when #areRequired meet not false value', function (done) {
+                shouldNotThrowError(function () {
+                    requireBoth(1, "some value");
+                    requireBoth(2, false);
+                    requireBoth(3, true);
+                    requireBoth(4, "");
+                    requireBoth(5, 0);
+                }, done);
+            });
+            it('should throw error when #areRequired meet false value', function (done) {
+                var falsyValus = [null, undefined, NaN]; // except zero, false and empty string
+                for(var i = 0; i < falsyValus.length; i++) {
+                    shouldThrowError(function(){
+                        requireBoth("some value", falsyValus[i]);
+                    });
+                }
+                for(var i = 0; i < falsyValus.length; i++) {
+                    shouldThrowError(function(){
+                        requireBoth(falsyValus[i], "some value");
+                    });
+                }
+                done();
+            });
+        }
     });
     describe('#notEmptyString', function() {
         function notEmpty1(argu1, argu2) {
-            $(arguments).notEmptyString(argu2);
+            $(arguments).isNotEmptyString(argu2);
         }
         it('should work fine when #notEmptyString meet not empty string value', function(done) {
-            try {
+            shouldNotThrowError(function () {
                 notEmpty1(null, "some value");
                 notEmpty1(null, " s ");
                 notEmpty1(null, 's ');
                 notEmpty1(null, ' s');
-                done();
-            } catch (e) {
-                expect(e).to.be.undefined;
-                throw e;
-            }
+            }, done);
         });
         it('should throw error when #notEmptyString meet empty string value', function (done) {
-            var falsyValus = [""
-                , "   "
-                , null
-                , undefined
-                , NaN
-                , true
-                , false
-                , 0
-                , 99
-                , []
-
-            ];
+            var falsyValus = ["", "   ", null, undefined, NaN, true, false, 0, 99, []];
             for(var i = 0; i < falsyValus.length; i++) {
-                var isError = false;
-                try {
+                shouldThrowError(function () {
                     notEmpty1("some value", falsyValus[i]);
-                    isError = true;
-                } catch (e) {
-                    expect(e).to.be.not.undefined;
-                }
-                expect(isError).to.equal(false);
+                });
             }
             done();
         });
     });
     describe('#notEmptyArray', function () {
         function notEmptyArray1(argu1, argu2) {
-            $(arguments).notEmptyArray(argu2);
+            $(arguments).isNotEmptyArray(argu2);
         }
         it('should work fine when #notEmptyArray meet not empty array value', function (done) {
-            try {
+            shouldNotThrowError(function () {
                 notEmptyArray1(null, ["some value"]);
                 notEmptyArray1(null, [false]);
                 notEmptyArray1(null, [true]);
@@ -110,23 +156,14 @@ describe('smarg.validators', function() {
                 notEmptyArray1(null, [0]);
                 notEmptyArray1(null, [null]);
                 notEmptyArray1(null, [undefined]);
-                done();
-            } catch (e) {
-                expect(e).to.be.undefined;
-                throw e;
-            }
+            }, done);
         });
         it('should throw error when #notEmptyArray meet empty array value', function (done) {
             var list = [[], undefined, NaN, 0, 99, true, false, null];
             for(var i = 0; i < list.length; i++) {
-                var isError = false;
-                try {
+                shouldThrowError(function () {
                     notEmptyArray1("some value", list[i]);
-                    isError = true;
-                } catch (e) {
-                    expect(e).to.be.not.undefined;
-                }
-                expect(isError).to.equal(false);
+                });
             }
             done();
         });
